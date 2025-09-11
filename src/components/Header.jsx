b8601e6d105a09bf72f602abd6ddd7f941bd8a46
corduka <cordukabdullah@gmail.com>
2025-09-11 11:00:19 -0500

    Set up Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import "../App.css";
 function Header() {
   return (
     <header>
@@ -7,7 +7,9 @@ function Header() {
         <img src="../src/assets/logo.png" />
       </Link>
       <div className="main-title">
-        <h1 className="color-white">Merge Monitor</h1>
+        <h1 className="text-blue-400 text-3xl font-bold underline">
+          Merge Monitor
+        </h1>
         <span>07:22 AM</span>
       </div>
     </header>
