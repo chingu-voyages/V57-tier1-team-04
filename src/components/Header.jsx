1c900efe35f94fae0a2443741bae155c3abe45e3
corduka <cordukabdullah@gmail.com>
2025-09-25 15:14:28 -0500

    Change design of the time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,16 @@
-import {useEffect, useState} from 'react';
+
 import { Link } from "react-router-dom";
 import "../App.css";
 function Header() {
-  const [time, setTime] = useState('');
 
-  useEffect(() => {
-    const updateClock = () => {
-      const now = new Date();
-      const options = {hour: '2-digit', minute: '2-digit', hour12:true};
-      setTime(now.toLocaleTimeString([], options));
-    };
-
-    updateClock(); //run once immediately
-    const timer = setInterval(updateClock, 1000); //update every second
-    return () => clearInterval(timer); //cleanup on unmount
-  }, []);
   return (
     <header>
-      <div className="header items-baseline">
-                      <Link to="/">
+      <Link to="/">
         <img className="logo" src="../src/assets/logo.png" />
       </Link> 
+      <div className="header-content items-baseline">
         <h1 className="main-heading">Pull Request DashBoard</h1>
-        <span>{time}</span>
-        
+
       </div>
     </header>
   );
