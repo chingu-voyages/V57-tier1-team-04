61724e9e02d8b958c0b613365e6469c36e7d5dcf
corduka <cordukabdullah@gmail.com>
2025-09-26 10:50:41 -0500

    Remove the unnecesary Route.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import OpenPRs from "./pages/OpenPRs";
 import ClosedPRs from "./pages/ClosedPRs";
 import Contributors from "./pages/Contributors";
 import NotFoundPage from "./pages/NotFoundPage";
-import PrListPage from "./pages/prFullBacklog";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
           <Route path="/open-prs" element={<OpenPRs />} />
           <Route path="/closed-prs" element={<ClosedPRs />} />
           <Route path="/contributors" element={<Contributors />} />
-          <Route path="/pr-list/:filter" element={<PrListPage/>}/>
+          <Route path="/about-us" element={<AboutUs />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </MainContent>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
+      fontFamily: {
+      smooch: ["'Smooch Sans'", "sans-serif"],
+    },
       colors: {
         primary: "#60B8DE",
       },
