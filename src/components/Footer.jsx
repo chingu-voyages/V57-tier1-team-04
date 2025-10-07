ab994c798953185a42ede5498405e1f31c02a4de
Nikoo Nasrpooya <nikoonasrpooya@yahoo.com>
2025-10-07 00:07:53 -0400

    logo address in footer is updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,44 @@
 function Footer() {
   return (
     <footer>
-      <img src="../src/assets/logo.png" alt="Team Blue Project Logo" /> 
+      <img src="../public/logo.png" alt="Team Blue Project Logo" />
       <p>
         Coded with 🩵 by <b>Team Blue</b>
       </p>
       <ul>
         <li className="team-member">
-          <a href="https://github.com/corduka" target="_blank"
-          aria-label="Abdullah's GitHub Profile"
+          <a
+            href="https://github.com/corduka"
+            target="_blank"
+            aria-label="Abdullah's GitHub Profile"
           >
             Abdullah
           </a>
         </li>
         <li className="team-member">
-          <a href="https://github.com/NikooNasrpooya" target="_blank"
-             aria-label="Nikoo's GitHub Profile"
-             >
+          <a
+            href="https://github.com/NikooNasrpooya"
+            target="_blank"
+            aria-label="Nikoo's GitHub Profile"
+          >
             Nikoo
           </a>
         </li>{" "}
         <li className="team-member">
-          <a href="https://github.com/CatYoung018" target="_blank"
-             aria-label="Cat's GitHub Profile"
-             >
+          <a
+            href="https://github.com/CatYoung018"
+            target="_blank"
+            aria-label="Cat's GitHub Profile"
+          >
             Cat
           </a>
         </li>{" "}
         <li className="team-member">
-          <a href="https://github.com/Gursimranb127" target="_blank"
-             aria-label="Gursimran's GitHub Profile"
-             >
+          <a
+            href="https://github.com/Gursimranb127"
+            target="_blank"
+            aria-label="Gursimran's GitHub Profile"
+          >
             Gursimran
           </a>
         </li>
